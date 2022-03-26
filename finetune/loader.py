317e940d64e0c0097860d9a20ff8fc135d28699a
Yijia-Xiao <xyj_thu@163.com>
2022-03-26 22:35:54 +0800

    remove loader TAPE

--- finetune/loader.py
+++ finetune/loader.py
@@ -1,254 +1,28 @@
-@registry.register_task('fluorescence')
-class FluorescenceDataset(Dataset):
+import argparse
+import math
+from tqdm import tqdm
+from Bio.SeqIO.FastaIO import Seq, SeqRecord
+from tape.datasets import LMDBDataset
 
-    def __init__(self,
-                 data_path: Union[str, Path],
-                 split: str,
-                 tokenizer: Union[str, TAPETokenizer] = 'iupac',
-                 in_memory: bool = False):
+parser = argparse.ArgumentParser(description='Convert an lmdb file into a fasta file')
+parser.add_argument('lmdbfile', type=str, help='The lmdb file to convert')
+parser.add_argument('fastafile', type=str, help='The fasta file to output')
+args = parser.parse_args()
 
-        if split not in ('train', 'valid', 'test'):
-            raise ValueError(f"Unrecognized split: {split}. "
-                             f"Must be one of ['train', 'valid', 'test']")
-        if isinstance(tokenizer, str):
-            tokenizer = TAPETokenizer(vocab=tokenizer)
-        self.tokenizer = tokenizer
+dataset = LMDBDataset(args.lmdbfile)
+fastafile = args.fastafile
+if not fastafile.endswith('.fasta'):
+    fastafile += '.fasta'
 
-        data_path = Path(data_path)
-        data_file = f'fluorescence/fluorescence_{split}.lmdb'
-        self.data = dataset_factory(data_path / data_file, in_memory)
+data = list()
 
-    def __len__(self) -> int:
-        return len(self.data)
+id_fill = math.ceil(math.log10(len(dataset)))
+for i, element in enumerate(tqdm(dataset)):
+    id_ = element.get('id', str(i).zfill(id_fill))
+    if isinstance(id_, bytes):
+        id_ = id_.decode()
 
-    def __getitem__(self, index: int):
-        item = self.data[index]
-        token_ids = self.tokenizer.encode(item['primary'])
-        input_mask = np.ones_like(token_ids)
-        return token_ids, input_mask, float(item['log_fluorescence'][0])
-
-    def collate_fn(self, batch: List[Tuple[Any, ...]]) -> Dict[str, torch.Tensor]:
-        input_ids, input_mask, fluorescence_true_value = tuple(zip(*batch))
-        input_ids = torch.from_numpy(pad_sequences(input_ids, 0))
-        input_mask = torch.from_numpy(pad_sequences(input_mask, 0))
-        fluorescence_true_value = torch.FloatTensor(fluorescence_true_value)  # type: ignore
-        fluorescence_true_value = fluorescence_true_value.unsqueeze(1)
-
-        return {'input_ids': input_ids,
-                'input_mask': input_mask,
-                'targets': fluorescence_true_value}
-
-
-@registry.register_task('stability')
-class StabilityDataset(Dataset):
-
-    def __init__(self,
-                 data_path: Union[str, Path],
-                 split: str,
-                 tokenizer: Union[str, TAPETokenizer] = 'iupac',
-                 in_memory: bool = False):
-
-        if split not in ('train', 'valid', 'test'):
-            raise ValueError(f"Unrecognized split: {split}. "
-                             f"Must be one of ['train', 'valid', 'test']")
-        if isinstance(tokenizer, str):
-            tokenizer = TAPETokenizer(vocab=tokenizer)
-        self.tokenizer = tokenizer
-
-        data_path = Path(data_path)
-        data_file = f'stability/stability_{split}.lmdb'
-
-        self.data = dataset_factory(data_path / data_file, in_memory)
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __getitem__(self, index: int):
-        item = self.data[index]
-        token_ids = self.tokenizer.encode(item['primary'])
-        input_mask = np.ones_like(token_ids)
-        return token_ids, input_mask, float(item['stability_score'][0])
-
-    def collate_fn(self, batch: List[Tuple[Any, ...]]) -> Dict[str, torch.Tensor]:
-        input_ids, input_mask, stability_true_value = tuple(zip(*batch))
-        input_ids = torch.from_numpy(pad_sequences(input_ids, 0))
-        input_mask = torch.from_numpy(pad_sequences(input_mask, 0))
-        stability_true_value = torch.FloatTensor(stability_true_value)  # type: ignore
-        stability_true_value = stability_true_value.unsqueeze(1)
-
-        return {'input_ids': input_ids,
-                'input_mask': input_mask,
-                'targets': stability_true_value}
-
-
-@registry.register_task('remote_homology', num_labels=1195)
-class RemoteHomologyDataset(Dataset):
-
-    def __init__(self,
-                 data_path: Union[str, Path],
-                 split: str,
-                 tokenizer: Union[str, TAPETokenizer] = 'iupac',
-                 in_memory: bool = False):
-
-        if split not in ('train', 'valid', 'test_fold_holdout',
-                         'test_family_holdout', 'test_superfamily_holdout'):
-            raise ValueError(f"Unrecognized split: {split}. Must be one of "
-                             f"['train', 'valid', 'test_fold_holdout', "
-                             f"'test_family_holdout', 'test_superfamily_holdout']")
-        if isinstance(tokenizer, str):
-            tokenizer = TAPETokenizer(vocab=tokenizer)
-        self.tokenizer = tokenizer
-
-        data_path = Path(data_path)
-        data_file = f'remote_homology/remote_homology_{split}.lmdb'
-        self.data = dataset_factory(data_path / data_file, in_memory)
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __getitem__(self, index: int):
-        item = self.data[index]
-        token_ids = self.tokenizer.encode(item['primary'])
-        input_mask = np.ones_like(token_ids)
-        return token_ids, input_mask, item['fold_label']
-
-    def collate_fn(self, batch: List[Tuple[Any, ...]]) -> Dict[str, torch.Tensor]:
-        input_ids, input_mask, fold_label = tuple(zip(*batch))
-        input_ids = torch.from_numpy(pad_sequences(input_ids, 0))
-        input_mask = torch.from_numpy(pad_sequences(input_mask, 0))
-        fold_label = torch.LongTensor(fold_label)  # type: ignore
-
-        return {'input_ids': input_ids,
-                'input_mask': input_mask,
-                'targets': fold_label}
-
-
-@registry.register_task('contact_prediction')
-class ProteinnetDataset(Dataset):
-
-    def __init__(self,
-                 data_path: Union[str, Path],
-                 split: str,
-                 tokenizer: Union[str, TAPETokenizer] = 'iupac',
-                 in_memory: bool = False):
-
-        if split not in ('train', 'train_unfiltered', 'valid', 'test'):
-            raise ValueError(f"Unrecognized split: {split}. Must be one of "
-                             f"['train', 'train_unfiltered', 'valid', 'test']")
-
-        if isinstance(tokenizer, str):
-            tokenizer = TAPETokenizer(vocab=tokenizer)
-        self.tokenizer = tokenizer
-
-        data_path = Path(data_path)
-        data_file = f'proteinnet/proteinnet_{split}.lmdb'
-        self.data = dataset_factory(data_path / data_file, in_memory)
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __getitem__(self, index: int):
-        item = self.data[index]
-        protein_length = len(item['primary'])
-        token_ids = self.tokenizer.encode(item['primary'])
-        input_mask = np.ones_like(token_ids)
-
-        valid_mask = item['valid_mask']
-        contact_map = np.less(squareform(pdist(item['tertiary'])), 8.0).astype(np.int64)
-
-        yind, xind = np.indices(contact_map.shape)
-        invalid_mask = ~(valid_mask[:, None] & valid_mask[None, :])
-        invalid_mask |= np.abs(yind - xind) < 6
-        contact_map[invalid_mask] = -1
-
-        return token_ids, input_mask, contact_map, protein_length
-
-    def collate_fn(self, batch: List[Tuple[Any, ...]]) -> Dict[str, torch.Tensor]:
-        input_ids, input_mask, contact_labels, protein_length = tuple(zip(*batch))
-        input_ids = torch.from_numpy(pad_sequences(input_ids, 0))
-        input_mask = torch.from_numpy(pad_sequences(input_mask, 0))
-        contact_labels = torch.from_numpy(pad_sequences(contact_labels, -1))
-        protein_length = torch.LongTensor(protein_length)  # type: ignore
-
-        return {'input_ids': input_ids,
-                'input_mask': input_mask,
-                'targets': contact_labels,
-                'protein_length': protein_length}
-
-
-@registry.register_task('secondary_structure', num_labels=3)
-class SecondaryStructureDataset(Dataset):
-
-    def __init__(self,
-                 data_path: Union[str, Path],
-                 split: str,
-                 tokenizer: Union[str, TAPETokenizer] = 'iupac',
-                 in_memory: bool = False):
-
-        if split not in ('train', 'valid', 'casp12', 'ts115', 'cb513'):
-            raise ValueError(f"Unrecognized split: {split}. Must be one of "
-                             f"['train', 'valid', 'casp12', "
-                             f"'ts115', 'cb513']")
-        if isinstance(tokenizer, str):
-            tokenizer = TAPETokenizer(vocab=tokenizer)
-        self.tokenizer = tokenizer
-
-        data_path = Path(data_path)
-        data_file = f'secondary_structure/secondary_structure_{split}.lmdb'
-        self.data = dataset_factory(data_path / data_file, in_memory)
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __getitem__(self, index: int):
-        item = self.data[index]
-        token_ids = self.tokenizer.encode(item['primary'])
-        input_mask = np.ones_like(token_ids)
-
-        # pad with -1s because of cls/sep tokens
-        labels = np.asarray(item['ss3'], np.int64)
-        labels = np.pad(labels, (1, 1), 'constant', constant_values=-1)
-
-        return token_ids, input_mask, labels
-
-    def collate_fn(self, batch: List[Tuple[Any, ...]]) -> Dict[str, torch.Tensor]:
-        input_ids, input_mask, ss_label = tuple(zip(*batch))
-        input_ids = torch.from_numpy(pad_sequences(input_ids, 0))
-        input_mask = torch.from_numpy(pad_sequences(input_mask, 0))
-        ss_label = torch.from_numpy(pad_sequences(ss_label, -1))
-
-        output = {'input_ids': input_ids,
-                  'input_mask': input_mask,
-                  'targets': ss_label}
-
-        return output
-
-
-# import argparse
-# import math
-# from tqdm import tqdm
-# from Bio.SeqIO.FastaIO import Seq, SeqRecord
-# from tape.datasets import LMDBDataset
-
-# parser = argparse.ArgumentParser(description='Convert an lmdb file into a fasta file')
-# parser.add_argument('lmdbfile', type=str, help='The lmdb file to convert')
-# parser.add_argument('fastafile', type=str, help='The fasta file to output')
-# args = parser.parse_args()
-
-# dataset = LMDBDataset(args.lmdbfile)
-# fastafile = args.fastafile
-# if not fastafile.endswith('.fasta'):
-#     fastafile += '.fasta'
-
-# data = list()
-
-# id_fill = math.ceil(math.log10(len(dataset)))
-# for i, element in enumerate(tqdm(dataset)):
-#     id_ = element.get('id', str(i).zfill(id_fill))
-#     if isinstance(id_, bytes):
-#         id_ = id_.decode()
-
-#     primary = element['primary']
-#     seq = Seq(primary)
-#     # record = SeqRecord(seq, id_)
-#     # outfile.write(record.format('fasta'))
+    primary = element['primary']
+    seq = Seq(primary)
+    # record = SeqRecord(seq, id_)
+    # outfile.write(record.format('fasta'))
--- megatron/data/msa_dataset.py
+++ megatron/data/msa_dataset.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""TAPE style dataset."""
+"""MSA style dataset."""
 
 import os
 import time
